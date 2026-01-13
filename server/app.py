a1ef7124dfc0b713370108cbdf66ba4bfa425d07
rainer wanjohi <rainerwanjohi384@gmail.com>
2026-01-13 10:04:35 +0300

    Implement PATCH and DELETE methods for PlantByID resource, fix GET to handle missing plants

--- server/app.py
+++ server/app.py
@@ -44,8 +44,32 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response({"error": "Plant not found"}, 404)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+            for key, value in data.items():
+                setattr(plant, key, value)
+            db.session.add(plant)
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response({"error": "Plant not found"}, 404)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response("", 204)
+        else:
+            return make_response({"error": "Plant not found"}, 404)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
